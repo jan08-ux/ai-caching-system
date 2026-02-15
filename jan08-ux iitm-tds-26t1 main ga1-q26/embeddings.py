bb306602b11391fc2090ee6b494756426947da04
jan08-ux <janjitha08@gmail.com>
2026-02-15 13:35:29 +0530

    Delete jan08-ux iitm-tds-26t1 main ga1-q26 directory

--- jan08-ux iitm-tds-26t1 main ga1-q26/embeddings.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
-
-def embed(text: str) -> np.ndarray:
-    np.random.seed(abs(hash(text)) % (10**8))
-    return np.random.randn(1, 384)
-
-def similarity(vec1, vec2):
-    return cosine_similarity(vec1, vec2)[0][0]
